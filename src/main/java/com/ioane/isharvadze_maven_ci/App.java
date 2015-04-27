31a089a5aad77440866e9632cdab1000722f4166
ioane5 <ioane.sharvadze@gmail.com>
2015-04-27 22:45:55 +0400

    task 6
    Added simple application

---
+++ src/main/java/com/ioane/isharvadze_maven_ci/App.java
@@ -0,0 +1,13 @@
+package com.ioane.isharvadze_maven_ci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
